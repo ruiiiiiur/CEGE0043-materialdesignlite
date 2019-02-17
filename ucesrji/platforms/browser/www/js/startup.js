177462a3b573e3e6ea54445a330d614b26e7cb40
ruiiiiiur <ruiiiiiur@gmail.com>
2019-02-17 15:21:57 -0500

    Track and Circle this time the code is more cleaner

--- ucesrji/platforms/browser/www/js/startup.js
+++ ucesrji/platforms/browser/www/js/startup.js
@@ -1,53 +1,8 @@
 function trackAndCircle(){
 
-function addPointLinePoly() { 
-// add a point
-L.marker([51.5, -0.09]).addTo(mymap)
-.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-// add a circle
-L.circle([51.508, -0.11], 500, {
-color: 'red',
-fillColor: '#f03',
-fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a circle.");
-// add a polygon with 3 end points (i.e. a triangle)
-var myPolygon = L.polygon([
-[51.509, -0.08],
-[51.503, -0.06],
-[51.51, -0.047]
-],{
-color: 'red',
-fillColor: '#f03',
-fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a polygon.");
-}
 
 addPointLinePoly();
-
-
-
-
-
-function trackLocation() {
-if (navigator.geolocation) {
-navigator.geolocation.watchPosition(showPosition);
-} else {
-document.getElementById('showLocation').innerHTML =
- "Geolocation is not supported by this browser.";
-}
-}
-
-
 var userMarker;
-
-function showPosition(position) {
-if (userMarker){mymap.removeLayer(userMarker);}
-
-userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
-
-document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-}
-
 trackLocation();
 
 
