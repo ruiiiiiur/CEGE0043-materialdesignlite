b308be3296ae4c7c9ba3c94463d0068003352755
ruiiiiiur <ruiiiiiur@gmail.com>
2019-02-17 15:34:27 -0500

    automatically run the track and circle function

--- ucesrji/www/js/startup.js
+++ ucesrji/www/js/startup.js
@@ -2,3 +2,13 @@ function trackAndCircle(){
 addPointLinePoly();
 trackLocation();   
 }
+
+
+function startup() {
+document.addEventListener('DOMContentLoaded', function() {
+trackAndCircle ();
+}, false);
+}
+
+
+
